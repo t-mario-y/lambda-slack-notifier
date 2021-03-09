df8f660311d6148e41d9dc52030301a621f4f7c7
t-mario-y <t.mario.yoshi@gmail.com>
2021-03-09 22:40:41 +0900

    feat: get Scrapbox access info from SSM parameters

--- serverless.ts
+++ serverless.ts
@@ -1,7 +1,7 @@
 import { AWS } from "@serverless/typescript";
 
 const serverlessConfig: AWS = {
-  service: "aws-nodejs-example",
+  service: "scrapbox-feeds",
   frameworkVersion: "2",
 
   provider: {
@@ -9,11 +9,17 @@ const serverlessConfig: AWS = {
     runtime: "nodejs14.x",
     lambdaHashingVersion: "20201221",
     region: "ap-northeast-1",
+    stage: "production",
   },
 
   functions: {
-    hello: {
+    main: {
       handler: "dist/app.handler",
+      environment: {
+        GET_SCRAPBOX_PAGES_ENDPOINT:
+          "${ssm:/scrapbox-feeds/get-scrapbox-pages-endpoint~true}",
+        SCRAPBOX_COOKIE: "${ssm:/scrapbox-feeds/scrapbox-cookie~true}",
+      },
     },
   },
 };
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import { Handler } from "aws-lambda";
 
 export const handler: Handler = async () => {
+  const scrapboxApiEndpoint = process.env["GET_SCRAPBOX_PAGES_ENDPOINT"];
+  const scrapboxCookie = process.env["SCRAPBOX_COOKIE"];
+
+  if (scrapboxApiEndpoint === undefined || scrapboxCookie === undefined) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `required environment variables are not set.`,
+      }),
+    };
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `Hello, world! Your function executed successfully!`,
+        message: `Hello, world! Your function executed successfully!
+        GET_SCRAPBOX_PAGES_ENDPOINT:${scrapboxApiEndpoint}
+        SCRAPBOX_COOKIE:${scrapboxCookie}
+        `,
       },
       null,
       2
